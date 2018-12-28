de5692a6de3b6a01f17b316c204dba3c901a994a
Abhi3685 <35190737+Abhi3685@users.noreply.github.com>
2018-12-28 20:31:15 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+var port = process.env.port || 3000;
+
+hbs.registerPartials('views/partials');
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: "Welcome to the homepage."
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About Page"
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help.hbs', {
+    pageTitle: "Help Page"
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact.hbs', {
+    pageTitle: "Contact us"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
