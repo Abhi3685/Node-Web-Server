b01eb741339a52fca2851e1a04851914ca8697e5
Abhi3685 <35190737+Abhi3685@users.noreply.github.com>
2018-12-28 21:33:19 +0530

    'Deploy'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 
 hbs.registerPartials('views/partials');
 app.set('view engine', 'hbs');
